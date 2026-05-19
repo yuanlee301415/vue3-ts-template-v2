b37254957badd8e64a2cc7defb5875db29404f7e
YuanLee <liyuan301415@126.com>
2026-05-19 21:10:03 +0800

    fix: 移除 Tailwind & 修复相关样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
+<script lang="ts" setup>
+defineOptions({ name: 'HomeView' })
+</script>
+
 <template>
-  <div>
+  <div class="home-view">
     <h1>Home</h1>
-    <div class="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-center space-x-4">
-      <div class="flex-shrink-0">
-        <img class="h-12 w-12" src="@/assets/logo.svg" alt="Logo" />
+    <div class="card-container">
+      <div class="card-icon">
+        <img src="@/assets/logo.svg" alt="Logo" />
       </div>
-      <div>
-        <div class="text-xl font-medium text-gray-400">ChitChat</div>
-        <p class="text-gray-500">You have a new message!</p>
+      <div class="card-content">
+        <div class="card-title">ChitChat</div>
+        <p class="card-desc">You have a new message!</p>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-defineOptions({ name: 'HomeView' })
-</script>
+<style scoped lang="less">
+.home-view {
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  .card-container {
+    padding: 1.5rem;
+    max-width: 24rem;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 0.75rem;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    .card-icon {
+      flex-shrink: 0;
+
+      img {
+        height: 3rem;
+        width: 3rem;
+      }
+    }
+
+    .card-content {
+      .card-title {
+        font-size: 1.25rem;
+        font-weight: 500;
+        color: #9ca3af;
+      }
+
+      .card-desc {
+        color: #6b7280;
+      }
+    }
+  }
+}
+</style>
